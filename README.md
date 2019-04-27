bcfb0239249f7b8813fdecada0b473dd1d0c1d6b
Raghu Datta <raghudatta@PLAP00000026.local>
2019-04-27 12:48:19 +0530

    Mobile Challenge API added

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ docker run -p 3000:3000 challenge:1.0
 
 curl localhost:3000
 Hello World
+
+#API for Mobile Challenge
+Go to https://editor.swagger.io/ and paste this JSON https://raw.githubusercontent.com/Prenetics/test-api/master/api.json
+
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,39 @@
 var appRouter = function(app) {
     app.get("/", function(req, res) {
         res.send("Hello World");
+});
+var bodyParser = require('body-parser');
+    app.use(bodyParser.json()); // support json encoded bodies
+ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+ app.post('/login', function(req, res) {
+        var user_id = req.body.user_id;
+        var password = req.body.token;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"token":"challengeToken"}));
+    });
+    app.get("/customer/challengeToken/user", function(req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"firstname":"Raghu","lastname":"Datta","email":"raghu.datta@prenetics.com","dob":"27-12-1990"}));
+    });
+    app.get("/customer/challengeToken/report", function(req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"report_url":"http://www.sraghudatta.com/customer/RaghuReport.pdf"}));
+    });
+    app.get("/customer/challengeToken/genetics", function(req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify([{"name":"Dummy Name1","symbol":"DummySymbol1"},{"name":"Dummy Name2","symbol":"DummySymbol2"},{"name":"Dummy Name3","symbol":"DummySymbol3"}]));
+    });
+    app.post('/customer/challengeToken/heartrate', function(req, res) {
+        var rate = req.body.rate;
+        var timestamp = req.body.timestamp;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"status":"heartrate_received"}));
+    });
+    app.post('/logout', function(req, res) {
+        var token = req.body.token;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({"status":"logged_out"}));
     });
 }
 
